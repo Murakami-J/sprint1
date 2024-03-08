55907912746aa557b4fb4822d4057a70b0ff264f
RyanTorres05 <ryan.costa@sptech.school>
2024-03-08 11:38:53 -0300

    alteração banco de dados Ryan

--- entregaveis/BD/Ryan_Torres_BD_Monitoramento_Vacina.sql
+++ entregaveis/BD/Ryan_Torres_BD_Monitoramento_Vacina.sql
@@ -19,4 +19,27 @@ idVacina int primary key auto_increment,
 nomeVacina varchar(50),
 dataFabricacao date,
 dataValidade date);
-﻿
+
+insert into login values 
+(default,'ryan@hotmail.com', 'abacaxi123', 'apple'),
+(default,'joao@hotmail.com', 'amorai123', 'google'),
+(default,'juliana@hotmail.com', 'maça123', 'amazon'),
+(default,'victor@hotmail.com', 'banana123i123', 'itau');
+
+select * from login;
+
+insert into monitoramento values
+(default,'10', '25', '2024-06-18 12:53:21'),
+(default,'9', '32', '2024-03-23 17:45:12'),
+(default,'5', '23', '2024-02-09 23:59:59'),
+(default,'-2', '24', '2024-05-02 00:15:21');
+
+select * from monitoramento;
+
+insert into vacina values 
+(default,'pfizer', '2024-06-18', '2025-06-18'),
+(default,'sarampo', '2024-06-18', '2025-06-18'),
+(default,'febre amarela', '2024-06-18', '2025-06-18'),
+(default,'HPV', '2024-06-18', '2025-06-18');
+
+select * from vacina;
