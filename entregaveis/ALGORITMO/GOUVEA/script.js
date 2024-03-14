0359d8115c78b9fb9f19ce22c00a40650aeacdd0
Gouvea <caua.nascimento@sptech.school>
2024-03-14 20:17:26 -0300

    Calculadora muito loca - Gouvea

---
+++ entregaveis/ALGORITMO/GOUVEA/script.js
@@ -0,0 +1,5 @@
+function calcularSolucao() {
+    var qntdEngrgas = Number(input_qntdEntregas.value)
+    var qntdCaixa = Number(input_capacidade.value)
+    var PrejuMensal = Number(input_prejuMensal.value)
+}
