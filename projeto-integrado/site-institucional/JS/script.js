2f55c1192469f70541dd173ae35ca7cd6aef5f45
Gouvea <caua.nascimento@sptech.school>
2024-03-18 11:21:26 -0300

    Novo visual calculadora

--- projeto-integrado/site-institucional/JS/script.js
+++ projeto-integrado/site-institucional/JS/script.js
@@ -1,10 +1,10 @@
 info.innerHTML =
 `<h3>💡Você Sabia?</h3><br>
-<p><i>De acordo com a Organização Mundial de Saúde (OMS), cerca de <b> 50% </b> das vacinas produzidas, em todo o mundo, chegam ao seu destino em condições precárias e
+<p>De acordo com a Organização Mundial de Saúde (OMS), cerca de <b> 50% </b> das vacinas produzidas, em todo o mundo, chegam ao seu destino em condições precárias e
 inadequadas para uso.</p><br>
 <p>Aproximadamente <b>40%</b> desses problemas decorrem principalmente por variações de temperatura durante o <b>transporte</b> de acordo com a Resolução da Anvisa
 (RDC Nº 430 -  Artigo 4º), diz que todas as partes envolvidas a produção, armazenagem, distribuição e <b>transporte</b> devem se responsabilizar pela qualidade e segurança dos
-medicamentos. O descumprimento das disposições constitui infração sanitária, levando à <b>multa</b>!<i></p>
+medicamentos. O descumprimento das disposições constitui infração sanitária, levando à <b>multa</b>!</p>
 `
 
 function calcularSolucao() {
@@ -18,6 +18,6 @@ function calcularSolucao() {
     var dinheiroPerdido = (valorVacina * vacinaTotalAno) * 0.4
 
 resolucao.innerHTML =
-`<p>Você está perdendo aproximadamente <b style=color:red>${vacinaTotalAno}</b> doses de vacinas por ano! Ou seja, <b style=color:red>R$${dinheiroPerdido}</b> por ano!</p><br>
-Ao utilizar nosso sensor inteligente, você poderá monitorar em tempo real a temperatura e umidade, podendo reduzir esse valor em até <b>40%</b> por variação de temperatura. <b>ADQUIRA JÁ!</b></p>`}
+`<p>De acordo com os dados divulgados pela Anvisa, suas perdas nas entregas por variação de temperatura dão aproximadamente <b style=color:red>${vacinaTotalAno}</b> doses de vacinas por ano! Ou seja, <b style=color:red>R$${dinheiroPerdido}</b> por ano!</p><br>
+Ao utilizar nosso sensor inteligente, você poderá monitorar em tempo real a temperatura e umidade, podendo reduzir esse valor em até <b>40%</b> por variação de temperatura. <a href="#" style="color: black"><b>ADQUIRA JÁ!</b></a></p>`}
 
