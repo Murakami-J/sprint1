7ec4f4ff4cd19c3a6f79797b00b4cd8be9f4a8ed
Cauã Gouvea <160280825+c-gouv@users.noreply.github.com>
2024-03-18 16:48:13 -0300

    Comentando o código do arduíno

--- projeto-integrado/DHT11_TemperatusCorp.ino
+++ projeto-integrado/DHT11_TemperatusCorp.ino
@@ -1,14 +1,18 @@
+// Declaração de variáveis e bibliotecas
 #include "DHT.h"
 #define dht_type DHT11
 
 int dht_pin = A1;
 DHT dht_1 = DHT(dht_pin, dht_type);
 
+
+// Sincronismo na conexão entre o arduino e a máquina
 void setup() {
   Serial.begin(9600);
   dht_1.begin();
 }
 
+// Função de loop para mostrar os dados na tela
 void loop() {
   float umidade = dht_1.readHumidity();
   float temperatura = dht_1.readTemperature();
