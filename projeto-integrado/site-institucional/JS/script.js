b1d1f8ae162290f9da7dba6d1b9f1029ec4a21d8
Gouvea <caua.nascimento@sptech.school>
2024-03-21 12:53:31 -0300

    Alteração para a facilitação de visualização do cloente

--- projeto-integrado/site-institucional/JS/script.js
+++ projeto-integrado/site-institucional/JS/script.js
@@ -1,11 +1,13 @@
-info.innerHTML =
-`<h3>💡Você Sabia?</h3><br>
-<p>De acordo com a Organização Mundial de Saúde (OMS), cerca de <b> 50% </b> das vacinas produzidas, em todo o mundo, chegam ao seu destino em condições precárias e
-inadequadas para uso.</p><br>
-<p>Aproximadamente <b>40%</b> desses problemas decorrem principalmente por variações de temperatura durante o <b>transporte</b> de acordo com a Resolução da Anvisa
-(RDC Nº 430 -  Artigo 4º), diz que todas as partes envolvidas a produção, armazenagem, distribuição e <b>transporte</b> devem se responsabilizar pela qualidade e segurança dos
-medicamentos. O descumprimento das disposições constitui infração sanitária, levando à <b>multa</b>!</p>
-`
+function voltar() {
+    content.innerHTML = `
+    
+    <div><p>Valor médio das vacinas:</p><input type="number" id="inputValorVacina"></div><br>
+    <div><p>Capacidade de vacinas por lote:</p><input type="number" id="inputQtdVacina"></div><br>
+    <div><p>Capacidade de lotes por entrega:</p><input type="number" id="inputQtdLote"></div><br>
+    <div><p>Quantidade de entregas no mês:</p><input type="number" id="inputQtdEntrega"></div><br>
+    <div class="bt"><button onclick="calcularSolucao()">CALCULAR</button><br></div>
+    `
+}
 
 function calcularSolucao() {
     var qtdEntrega = Number(inputQtdEntrega.value)
@@ -16,8 +18,13 @@ function calcularSolucao() {
     var vacinaTotalAno = ((qtdVacina * qtdLote) * qtdEntrega) * 12
     // var vacinaPerdida = vacinaTotalAno * 0.4
     var dinheiroPerdido = (valorVacina * vacinaTotalAno) * 0.4
-
-resolucao.innerHTML =
-`<p>De acordo com os dados divulgados pela Anvisa, suas perdas nas entregas por variação de temperatura dão aproximadamente <b style=color:red>${vacinaTotalAno}</b> doses de vacinas por ano! Ou seja, <b style=color:red>R$${dinheiroPerdido}</b> por ano!</p><br>
-Ao utilizar nosso sensor inteligente, você poderá monitorar em tempo real a temperatura e umidade, podendo reduzir esse valor em até <b>40%</b> por variação de temperatura. <a href="#" style="color: black"><b>ADQUIRA JÁ!</b></a></p>`}
-
+    content.innerHTML = `<div id="resolucao"><p>De acordo com os dados divulgados pela Anvisa, suas perdas nas entregas por variação de temperatura dão aproximadamente <b style=color:red>${vacinaTotalAno}</b> doses de vacinas por ano! Ou seja, <b style=color:red>R$${dinheiroPerdido}</b> por ano!</p><br>
+    Ao utilizar nosso sensor inteligente, você poderá monitorar em tempo real a temperatura e umidade, podendo reduzir esse valor em até <b>40%</b> por variação de temperatura. <a href="#" style="color: black"><b>ADQUIRA JÁ!</b></a></p><br></div>
+    <div class="infoBox" id="info"><h3>💡Você Sabia?</h3><br>
+    <p>De acordo com a Organização Mundial de Saúde (OMS), cerca de <b> 50% </b> das vacinas produzidas, em todo o mundo, chegam ao seu destino em condições precárias e
+    inadequadas para uso.</p><br>
+    <p>Aproximadamente <b>40%</b> desses problemas decorrem principalmente por variações de temperatura durante o <b>transporte</b> de acordo com a Resolução da Anvisa
+    (RDC Nº 430 -  Artigo 4º), diz que todas as partes envolvidas a produção, armazenagem, distribuição e <b>transporte</b> devem se responsabilizar pela qualidade e segurança dos
+    medicamentos. O descumprimento das disposições constitui infração sanitária, levando à <b>multa</b>!</p></div>
+    <br><div class="bt"><button onclick="voltar()">VOLTAR</button><br></div>`
+    }
